07fa740148fc9f2eccd54e68e5e791115ce3198f
Mansour13737 <mansournosrati69@gmail.com>
2025-06-08 15:28:30 +0330

    last4

--- fast-food/src/app/page.tsx
+++ fast-food/src/app/page.tsx
@@ -3,14 +3,19 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div>
-      <Link href={'/main'}>
-        <div className="h-screen w-full bg-gradient-to-b from-[#FF939B] to-[#EF2A39] relative flex justify-center">
-          <h2 className="font-normal font-lobster absolute top-[220px] text-[60px] text-white">Foodgo</h2>
-          <Image src={'/home2.png'} width={150} height={150} alt="Image 1" className="absolute bottom-0 left-0" />
-          <Image src={'/home1.png'} width={150} height={150} alt="Image 1" className="absolute bottom-0 left-[90px]" />
-        </div>
-
-      </Link>
+      <div className="h-screen w-full bg-gradient-to-b from-[#FF939B] to-[#EF2A39] relative flex justify-center">
+        <h2 className="font-normal font-lobster absolute top-[220px] text-[60px] text-white">Foodgo</h2>
+        <Link href={'/main'} className="absolute bottom-0 left-0">
+          <div className="w-[200px] h-[200px] flex items-center justify-center cursor-pointer">
+            <Image src={'/home2.png'} width={150} height={150} alt="Image 1" />
+          </div>
+        </Link>
+        <Link href={'/main'} className="absolute bottom-0 left-[90px]">
+          <div className="w-[200px] h-[200px] flex items-center justify-center cursor-pointer">
+            <Image src={'/home1.png'} width={150} height={150} alt="Image 1" />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
