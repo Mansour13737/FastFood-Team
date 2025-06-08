e94f8c5926a7779c646d06d09557d586013251e8
Mansour13737 <mansournosrati69@gmail.com>
2025-06-09 02:40:28 +0330

    commited3

--- fast-food/src/app/main/page.tsx
+++ fast-food/src/app/main/page.tsx
@@ -17,28 +17,34 @@ interface Product {
 export default function Main() {
 
   return (
-    <div className="h-screen w-full bg-white flex flex-col relative items-center px-4">
-      <h2 className="font-normal font-lobster absolute text-[35px] text-[#3C2F2F] top-[28px] left-[19px]">Foodgo</h2>
-      <p className="absolute tracking-tighter top-[89px] left-[19px] text-[15px] text-[#6A6A6A]">Order your favourite food <b className="text-[15px]">!</b></p>
-      <Image src={'/profile-photo.jpg'} width={70} height={70} alt="Pic" className="absolute top-[28px] right-[19px]" />
-      <div className="flex h-fit w-full  mt-[140px] items-center justify-between">
-        <input type="text" className="shadow-[0.5px_0.5px_0.5px_0.5px_rgba(0,0,0,0.3)] h-[40px] w-[80%] rounded-[15px] " />
-        <svg className="bg-[url('/setting.svg')] bg-contain bg-no-repeat bg-center w-[45px] h-[45px] " />
-      </div>
-      <div className="mt-8 flex gap-1">
-        <Button color="bg-[#EF2A39] text-white">All</Button>
-        <Button>Combos</Button>
-        <Button>Spicy</Button>
-        <Button>Hot</Button>
-      </div>
-      <div className="grid grid-cols-2 grid-rows-2 gap-[5px] w-full mt-3 mb-[120px] text-[#3C2F2F]">
-        {products.map((item: Product, key) => (
-          <ProductTitle info={item} key={key} />
-        ))}
-      </div>
-      <div className="w-full h-screen">
-      </div>
-      <div className="fixed bottom-0 left-0 right-0 bg-[url('/main-footer.png')] bg-cover bg-center bg-no-repeat w-full h-[50px] justify-between flex z-50">
+    <div className="min-h-screen w-full bg-white flex flex-col relative items-center px-4">
+      <header className="w-full relative h-[120px]">
+        <h2 className="font-normal font-lobster absolute text-[35px] text-[#3C2F2F] top-[28px] left-[19px]">Foodgo</h2>
+        <p className="absolute tracking-tighter top-[89px] left-[19px] text-[15px] text-[#6A6A6A]">Order your favourite food <b className="text-[15px]">!</b></p>
+        <Image src={'/profile-photo.jpg'} width={70} height={70} alt="Pic" className="absolute top-[28px] right-[19px]" />
+      </header>
+
+      <main className="w-full flex-1 flex flex-col">
+        <div className="flex h-fit w-full items-center justify-between">
+          <input type="text" className="shadow-[0.5px_0.5px_0.5px_0.5px_rgba(0,0,0,0.3)] h-[40px] w-[80%] rounded-[15px]" />
+          <svg className="bg-[url('/setting.svg')] bg-contain bg-no-repeat bg-center w-[45px] h-[45px]" />
+        </div>
+
+        <div className="mt-8 flex gap-1">
+          <Button color="bg-[#EF2A39] text-white">All</Button>
+          <Button>Combos</Button>
+          <Button>Spicy</Button>
+          <Button>Hot</Button>
+        </div>
+
+        <div className="grid grid-cols-2 grid-rows-2 gap-[5px] w-full mt-3 mb-[120px] text-[#3C2F2F]">
+          {products.map((item: Product, key) => (
+            <ProductTitle info={item} key={key} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="fixed bottom-0 left-0 right-0 bg-[url('/main-footer.png')] bg-cover bg-center bg-no-repeat w-full h-[50px] justify-between flex z-50">
         <div className="w-[38%] h-full flex justify-between items-center gap-10 px-[22px]">
           <Link href={'/'} className="w-fit h-fit">
             <Image src={'/home.png'} alt="home" width={17} height={17} />
@@ -55,8 +61,9 @@ export default function Main() {
             <Image src={'/heart.png'} alt="home" width={17} height={17} />
           </Link>
         </div>
-      </div>
-      <div className="w-full h-fit flex justify-center items-center absolute mx-auto bottom-15">
+      </footer>
+
+      <div className="w-full h-fit flex justify-center items-center fixed mx-auto bottom-[70px] z-50">
         <Link href={'/'}>
           <Image src={'/plus.png'} width={80} height={80} alt="plus" />
         </Link>
