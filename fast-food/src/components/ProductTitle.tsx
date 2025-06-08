00d6e56ca76fb809feb54e5f9f5056614dc5f451
Mansour13737 <mansournosrati69@gmail.com>
2025-06-09 01:59:49 +0330

    main-finished

--- fast-food/src/components/ProductTitle.tsx
+++ fast-food/src/components/ProductTitle.tsx
@@ -12,7 +12,7 @@ interface Product {
 export default function ProductTitle({ info }: { info: Product }) {
     return (
         <div className="relative flex flex-col items-center justify-between shadow-[0.5px_0.5px_1px_rgba(0,0,0,0.3)] rounded-[15px] h-[180px] px-[10px]">
-            <Image src={info.image} width={100} height={100} alt={info.title} />
+            <Image src={info.image} width={100} height={100} alt={info.title} priority />
 
             <div className="flex flex-col items-center pb-2">
                 <p className="w-[120px] text-wrap text-[#3C2F2F]  text-[12px]">{info.title}</p>
@@ -21,7 +21,7 @@ export default function ProductTitle({ info }: { info: Product }) {
                         <svg className=" bg-[url('/star.png')] bg-no-repeat bg-contain bg-center w-4 h-4" />
                         <span className="text-[12px]">{info.star}</span>
                     </div>
-                    <svg className=" bg-[url('/heart.svg')] bg-no-repeat bg-contain bg-center w-4 h-4" />
+                    <svg className=" lazy bg-[url('/heart.svg')] bg-no-repeat bg-contain bg-center w-4 h-4" />
                 </div>
             </div>
         </div>
