85488ac936aa4362c34273fd410a9fa759e1bf76
Mansour13737 <mansournosrati69@gmail.com>
2025-06-10 20:51:18 +0330

    1

--- fast-food/next.config.js
+++ fast-food/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: ['your-image-domain.com'], // دامنه تصاویر خارجی را اینجا اضافه کنید
   },
 }
