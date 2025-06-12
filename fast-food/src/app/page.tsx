708f4636b595d02d56e97763cd2721856d9b7b97
Mansour13737 <mansournosrati69@gmail.com>
2025-06-12 16:52:15 +0330

    3

--- fast-food/src/app/page.tsx
+++ fast-food/src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <Link href={'/main'}>
         <div className="h-screen w-full bg-gradient-to-b from-[#FF939B] to-[#EF2A39] relative flex justify-center">
           <h2 className="font-normal font-lobster absolute top-[220px] text-[60px] text-white">Foodgo</h2>
-          <Image src={'/home2.png'} width={150} height={150} alt="Image 1" className="absolute bottom-20 left-0" loading="lazy" />
-          <Image src={'/home1.png'} width={150} height={150} alt="Image 1" className="absolute bottom-20 left-[90px]" loading="lazy"/>
+          <Image src={'/home2.png'} width={150} height={150} alt="Image 1" className="absolute bottom-10 left-0" loading="lazy" />
+          <Image src={'/home1.png'} width={150} height={150} alt="Image 1" className="absolute bottom-10 left-[90px]" loading="lazy"/>
         </div>
       </Link>
     </>
